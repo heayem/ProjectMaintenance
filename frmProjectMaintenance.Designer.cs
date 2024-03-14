fe37527b2b29944c23cd971fc53586cb0de80f33
heayem <heayem7@gmail.com>
2024-03-14 19:54:53 +0700

    Add project files.

---
+++ frmProjectMaintenance.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace ProjectMaintenance
+{
+    partial class frmProjectMaintenance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnAddProduct = new System.Windows.Forms.Button();
+            this.btnDelProduct = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(485, 329);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnAddProduct
+            // 
+            this.btnAddProduct.Location = new System.Drawing.Point(503, 12);
+            this.btnAddProduct.Name = "btnAddProduct";
+            this.btnAddProduct.Size = new System.Drawing.Size(133, 23);
+            this.btnAddProduct.TabIndex = 1;
+            this.btnAddProduct.Text = "Add Product...";
+            this.btnAddProduct.UseVisualStyleBackColor = true;
+            // 
+            // btnDelProduct
+            // 
+            this.btnDelProduct.Location = new System.Drawing.Point(503, 41);
+            this.btnDelProduct.Name = "btnDelProduct";
+            this.btnDelProduct.Size = new System.Drawing.Size(133, 23);
+            this.btnDelProduct.TabIndex = 2;
+            this.btnDelProduct.Text = "Delete Product...";
+            this.btnDelProduct.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(503, 70);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(133, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // frmProjectMaintenance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 349);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelProduct);
+            this.Controls.Add(this.btnAddProduct);
+            this.Controls.Add(this.listBox1);
+            this.Name = "frmProjectMaintenance";
+            this.Text = "Project Maintenance ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnAddProduct;
+        private System.Windows.Forms.Button btnDelProduct;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
